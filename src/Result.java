81c07b5a3d5722913a1bb5cf82bb3ca049ee2832
Jesse Wagner <jesse.wagner@intel.com>
2015-05-30 13:47:39 -0700

    finished implementing the changes to the paint method, its a little more streamlined, I am also experimenting with using a logarithmic equation to calculate the distance based on the signal, it is only in the LOS method

--- src/Result.java
+++ src/Result.java
@@ -5,15 +5,21 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.lang.Double;
 
 public class Result extends JFrame {
 
-  private int x1, x2, x3, y1, y2, y3, r1, r2, r3;
+  private int x1, x2, x3, y1, y2, y3, d1, d2, d3, r1, r2, r3;
+  private int pixelsToFeet = 20;
   private boolean los;
+  private double slopeLOS = -5.935;
+  private double slopeNoLOS = -0.2827;
+  private double yInterceptLOS = 33.459;
+  private double yInterceptNoLOS = 59.808;
 
   Result() {
     super();
-    x1 = x2 = x3 = y1 = y2 = y3 = r1 = r2 = r3 = 0;
+    x1 = x2 = x3 = y1 = y2 = y3 = d1 = d2 = d3 = r1 = r2 = r3 = 0;
     los = false;
   }
 
@@ -34,17 +40,34 @@ public class Result extends JFrame {
   @Override
   public void paint(Graphics g) {
     Graphics2D g2d = (Graphics2D) g;
+
+    System.out.print(d1 + " " + d2 + " " + d3);
     if (los) {
-      g2d.drawOval(x1, y1, -(r1), -(r1));
-      g2d.drawOval(x2, y2, -(r2), -(r2));
-      g2d.drawOval(x3, y3, -(r2), -(r2));
+      d1 = distanceCalcLOS(r1);
+      d2 = distanceCalcLOS(r2);
+      d3 = distanceCalcLOS(r3);
     }
     else {
-      g2d.drawOval(x1, y1, -(r1)/2, -(r1)/2);
-      g2d.drawOval(x2, y2, -(r2)/2, -(r2)/2);
-      g2d.drawOval(x3, y3, -(r2)/2, -(r2)/2);
+      d1 = distanceCalcNoLOS(r1);
+      d2 = distanceCalcNoLOS(r2);
+      d3 = distanceCalcNoLOS(r3);
     }
+    g2d.drawOval(x1-(d1/2), y1-(d1/2), d1, d1);
+    g2d.drawOval(x2-(d2/2), y2-(d2/2), d2, d2);
+    g2d.drawOval(x3-(d3/2), y3-(d3/2), d3, d3);
   }
 
+  private int distanceCalcLOS(int signal)
+  {
+    double distance = (signal + yInterceptLOS) / slopeLOS;
+    distance = Math.exp(distance);
 
+    return (int) distance * pixelsToFeet;
+  }
+
+  private int distanceCalcNoLOS(int signal)
+  {
+    double distance = (signal + yInterceptNoLOS) / -slopeNoLOS;
+    return (int) distance * pixelsToFeet;
+  }
 }
