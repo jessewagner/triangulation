890bb8aef3da5853270d342999bcbf3335b2298c
Jesse Wagner <jesse.wagner@intel.com>
2015-06-01 20:36:36 -0700

    copied in brads changes to add a grid and color coding of circles

--- src/Result.java
+++ src/Result.java
@@ -1,5 +1,5 @@
 /**
- * Created by Brad on 5/16/15.
+ * Created by Brad and Jesse on 5/16/15.
  */
 
 import java.awt.*;
@@ -9,7 +9,7 @@ import java.lang.Double;
 
 public class Result extends JFrame {
 
-  private int x1, x2, x3, y1, y2, y3, d1, d2, d3, r1, r2, r3;
+  private int wid, len, x1, x2, x3, y1, y2, y3, d1, d2, d3, r1, r2, r3;
   private int pixelsToFeet = 20;
   private boolean los;
   private double slopeLOS = -5.935;
@@ -19,12 +19,14 @@ public class Result extends JFrame {
 
   Result() {
     super();
-    x1 = x2 = x3 = y1 = y2 = y3 = d1 = d2 = d3 = r1 = r2 = r3 = 0;
+    wid = len = x1 = x2 = x3 = y1 = y2 = y3 = d1 = d2 = d3 = r1 = r2 = r3 = 0;
     los = false;
   }
 
-  Result(int x, int xx, int xxx, int y, int yy, int yyy, int r, int rr, int rrr, boolean lineOfSight) {
+  Result(int w, int l, int x, int xx, int xxx, int y, int yy, int yyy, int r, int rr, int rrr, boolean lineOfSight) {
     //super();
+    wid = w * pixelsToFeet;
+    len = l * pixelsToFeet;
     x1 = x * pixelsToFeet;
     x2 = xx * pixelsToFeet;
     x3 = xxx * pixelsToFeet;
@@ -41,7 +43,14 @@ public class Result extends JFrame {
   public void paint(Graphics g) {
     Graphics2D g2d = (Graphics2D) g;
 
-    System.out.print(d1 + " " + d2 + " " + d3);
+    for (int i = 0; i < len; i += 100) {
+      g2d.drawLine(0, i, wid, i);
+    }
+    for (int i = 0; i < wid; i += 100) {
+      g2d.drawLine(i, 0, i, len);
+    }
+
+
     if (los) {
       d1 = distanceCalcLOS(r1);                                             //calls the distanceCalcLOS method to calculate
       d2 = distanceCalcLOS(r2);                                             //the distance from the signal
@@ -52,12 +61,16 @@ public class Result extends JFrame {
       d2 = distanceCalcNoLOS(r2);                                           //the distance from the signal
       d3 = distanceCalcNoLOS(r3);
     }
-    g2d.drawOval(x1-(d1/2), y1-(d1/2), d1, d1);                             //draws the first circle to indicate signal strength
-    g2d.drawOval(x1-(d1/60), y1-(d1/60), d1/30, d1/30);                     //draws the location the signal was recorded from
-    g2d.drawOval(x2-(d2/2), y2-(d2/2), d2, d2);                             //draws the second circle to indicate signal strength
-    g2d.drawOval(x2-(d2/60), y2-(d2/60), d2/30, d2/30);                     //draws the location the signal was recorded from
-    g2d.drawOval(x3-(d3/2), y3-(d3/2), d3, d3);                             //draws the third circle to indicate signal strength
-    g2d.drawOval(x3-(d3/60), y3-(d3/60), d3/30, d3/30);                     //draws the location the signal was recorded from
+    System.out.print(d1 + " " + d2 + " " + d3);
+    g2d.setColor(Color.GREEN);
+    g2d.drawOval(x1 - (d1 / 2), y1 - (d1 / 2), d1, d1);                     //draws the first circle to indicate signal strength
+    g2d.fillOval(x1 - (d1 / 60), y1 - (d1 / 60), d1 / 30, d1 / 30);         //draws the location the signal was recorded from
+    g2d.setColor(Color.BLUE);
+    g2d.drawOval(x2 - (d2 / 2), y2 - (d2 / 2), d2, d2);                     //draws the second circle to indicate signal strength
+    g2d.fillOval(x2 - (d2 / 60), y2 - (d2 / 60), d2 / 30, d2 / 30);         //draws the location the signal was recorded from
+    g2d.setColor(Color.RED);
+    g2d.drawOval(x3 - (d3 / 2), y3 - (d3 / 2), d3, d3);                     //draws the third circle to indicate signal strength
+    g2d.fillOval(x3 - (d3 / 60), y3 - (d3 / 60), d3 / 30, d3 / 30);         //draws the location the signal was recorded from
   }
 
   private int distanceCalcLOS(int signal)                                   //uses a logarithmic formula to calculate
@@ -70,7 +83,7 @@ public class Result extends JFrame {
 
   private int distanceCalcNoLOS(int signal)                                 //uses a logarithmic formula to calculate
   {                                                                         //the distance to a signal
-    double distance = (signal + yInterceptNoLOS) / -slopeNoLOS;
+    double distance = (signal + yInterceptNoLOS) / slopeNoLOS;
     distance = Math.exp(distance);
 
     return (int) distance * pixelsToFeet;
--- src/Triangulation.java
+++ src/Triangulation.java
@@ -1,5 +1,5 @@
 /**
- * Created by Brad on 5/16/15.
+ * Created by Brad and Jesse on 5/16/15.
  */
 import java.awt.*;
 import java.awt.event.*;
@@ -8,130 +8,131 @@ import javax.swing.*;
 // A Swing GUI application inherits from top-level container javax.swing.JFrame
 public class Triangulation extends JFrame {
 
-// private variables
+  // private variables
   private JTextField tfWidth, tfLength, tfX1, tfX2, tfX3, tfY1, tfY2, tfY3, tfRead1, tfRead2, tfRead3;
   private JCheckBox lineOfSight;
   private int wid, len;
 
-  private int count = 0;
   private int pixelsToFeet = 20;
 
-/** Constructor to setup the GUI components */
-public Triangulation() {
-  Container cp = getContentPane();
+  /** Constructor to setup the GUI components */
+  public Triangulation() {
+    Container cp = getContentPane();
 
-  // Content-pane sets layout
-  cp.setLayout(new GridLayout(13,1));
+    // Content-pane sets layout
+    cp.setLayout(new GridLayout(13,1));
 
-  cp.add(new JLabel("Room Width"));
-  tfWidth = new JTextField(10);
-  tfWidth.setEditable(true);
-  cp.add(tfWidth);
+    cp.add(new JLabel("Room Width"));
+    tfWidth = new JTextField(10);
+    tfWidth.setEditable(true);
+    cp.add(tfWidth);
 
-  cp.add(new JLabel("Room Length"));
-  tfLength = new JTextField(10);
-  tfLength.setEditable(true);
-  cp.add(tfLength);
+    cp.add(new JLabel("Room Length"));
+    tfLength = new JTextField(10);
+    tfLength.setEditable(true);
+    cp.add(tfLength);
 
-  cp.add(new JLabel("X-position 1"));
-  tfX1 = new JTextField(10);
-  tfX1.setEditable(true);
-  cp.add(tfX1);
+    cp.add(new JLabel("X-position 1"));
+    tfX1 = new JTextField(10);
+    tfX1.setEditable(true);
+    cp.add(tfX1);
 
-  cp.add(new JLabel("Y-position 1"));
-  tfY1 = new JTextField(10);
-  tfY1.setEditable(true);
-  cp.add(tfY1);
+    cp.add(new JLabel("Y-position 1"));
+    tfY1 = new JTextField(10);
+    tfY1.setEditable(true);
+    cp.add(tfY1);
 
-  cp.add(new JLabel("Reading 1"));
-  tfRead1 = new JTextField(10);
-  tfRead1.setEditable(true);
-  cp.add(tfRead1);
+    cp.add(new JLabel("Reading 1"));
+    tfRead1 = new JTextField(10);
+    tfRead1.setEditable(true);
+    cp.add(tfRead1);
 
-  cp.add(new JLabel("X-position 2"));
-  tfX2 = new JTextField(10);
-  tfX2.setEditable(true);
-  cp.add(tfX2);
+    cp.add(new JLabel("X-position 2"));
+    tfX2 = new JTextField(10);
+    tfX2.setEditable(true);
+    cp.add(tfX2);
 
-  cp.add(new JLabel("Y-position 2"));
-  tfY2 = new JTextField(10);
-  tfY2.setEditable(true);
-  cp.add(tfY2);
+    cp.add(new JLabel("Y-position 2"));
+    tfY2 = new JTextField(10);
+    tfY2.setEditable(true);
+    cp.add(tfY2);
 
-  cp.add(new JLabel("Reading 2"));
-  tfRead2 = new JTextField(10);
-  tfRead2.setEditable(true);
-  cp.add(tfRead2);
+    cp.add(new JLabel("Reading 2"));
+    tfRead2 = new JTextField(10);
+    tfRead2.setEditable(true);
+    cp.add(tfRead2);
 
-  cp.add(new JLabel("X-position 3"));
-  tfX3 = new JTextField(10);
-  tfX3.setEditable(true);
-  cp.add(tfX3);
+    cp.add(new JLabel("X-position 3"));
+    tfX3 = new JTextField(10);
+    tfX3.setEditable(true);
+    cp.add(tfX3);
 
-  cp.add(new JLabel("Y-position 3"));
-  tfY3 = new JTextField(10);
-  tfY3.setEditable(true);
-  cp.add(tfY3);
+    cp.add(new JLabel("Y-position 3"));
+    tfY3 = new JTextField(10);
+    tfY3.setEditable(true);
+    cp.add(tfY3);
 
-  cp.add(new JLabel("Reading 3"));
-  tfRead3 = new JTextField(10);
-  tfRead3.setEditable(true);
-  cp.add(tfRead3);
+    cp.add(new JLabel("Reading 3"));
+    tfRead3 = new JTextField(10);
+    tfRead3.setEditable(true);
+    cp.add(tfRead3);
 
-  //cp.add(new JLabel("Line of Sight?"));
-  lineOfSight = new JCheckBox("Line of Sight?");
-  cp.add(lineOfSight);
+    //cp.add(new JLabel("Line of Sight?"));
+    lineOfSight = new JCheckBox("Line of Sight?");
+    cp.add(lineOfSight);
 
-  // Content-pane adds components
-  JButton btnCalculate = new JButton("Calculate");
-  cp.add(btnCalculate);
+    // Content-pane adds components
+    JButton btnCalculate = new JButton("Calculate");
+    cp.add(btnCalculate);
 
-  // Source object adds listener
-  btnCalculate.addActionListener(new ActionListener() {
-    @Override
-    public void actionPerformed(ActionEvent e) {
-     // JFrame frame = new JFrame("Results");
-      Result r = new Result(Integer.parseInt(tfX1.getText()),
-                            Integer.parseInt(tfX2.getText()),
-                            Integer.parseInt(tfX3.getText()),
-                            Integer.parseInt(tfY1.getText()),
-                            Integer.parseInt(tfY2.getText()),
-                            Integer.parseInt(tfY3.getText()),
-                            Integer.parseInt(tfRead1.getText()),
-                            Integer.parseInt(tfRead2.getText()),
-                            Integer.parseInt(tfRead3.getText()),
-                            lineOfSight.isSelected());
-      wid = Integer.parseInt(tfWidth.getText());
-      len = Integer.parseInt(tfLength.getText());
-      //r.setSize(wid, len);
-      r.setPreferredSize(new Dimension(wid * pixelsToFeet, len * pixelsToFeet));
-      r.pack();
-      r.setVisible(true);
-      r.setTitle("Results");
-      r.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    // Source object adds listener
+    btnCalculate.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        // JFrame frame = new JFrame("Results");
+        Result r = new Result(Integer.parseInt(tfWidth.getText()),
+                Integer.parseInt(tfLength.getText()),
+                Integer.parseInt(tfX1.getText()),
+                Integer.parseInt(tfX2.getText()),
+                Integer.parseInt(tfX3.getText()),
+                Integer.parseInt(tfY1.getText()),
+                Integer.parseInt(tfY2.getText()),
+                Integer.parseInt(tfY3.getText()),
+                Integer.parseInt(tfRead1.getText()),
+                Integer.parseInt(tfRead2.getText()),
+                Integer.parseInt(tfRead3.getText()),
+                lineOfSight.isSelected());
+        wid = Integer.parseInt(tfWidth.getText());
+        len = Integer.parseInt(tfLength.getText());
+        //r.setSize(wid, len);
+        r.setPreferredSize(new Dimension(wid * pixelsToFeet, len * pixelsToFeet));
+        r.pack();
+        r.setVisible(true);
+        r.setTitle("Results");
+        r.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
 
-    }
-  });
+      }
+    });
 
-  setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  // Exit the program when the close-window button clicked
-  setTitle("Wifi Triangulation");  // "this" JFrame sets title
-  setSize(400, 400);   // "this" JFrame sets initial size (or pack())
-  setVisible(true);    // show it
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    // Exit the program when the close-window button clicked
+    setTitle("Wifi Triangulation");  // "this" JFrame sets title
+    setSize(400, 400);   // "this" JFrame sets initial size (or pack())
+    setVisible(true);    // show it
   }
 
 
 
-/** The entry main() method */
-public static void main(String[] args) {
-  // Run GUI codes in Event-Dispatching thread for thread-safety
-  SwingUtilities.invokeLater(new Runnable() {
+  /** The entry main() method */
+  public static void main(String[] args) {
+    // Run GUI codes in Event-Dispatching thread for thread-safety
+    SwingUtilities.invokeLater(new Runnable() {
 
-    @Override
-    public void run() {
-      new Triangulation();  // Let the constructor do the job
-    }
-  });
-}
+      @Override
+      public void run() {
+        new Triangulation();  // Let the constructor do the job
+      }
+    });
+  }
 }
